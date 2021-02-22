83f0e9682173a292fd531d8d5981604a61c9f830
unfinishedprogram <noah.labrecque@gmail.com>
2021-02-22 13:16:10 -0500

    Added hadsRequirements and hasComponent functions to the component class

--- src/Components/Component.ts
+++ src/Components/Component.ts
@@ -1,6 +1,8 @@
+import { Mesh } from "three";
+import { Transform } from "./Transform";
 export abstract class Component {
     required_components: Component[];
-
+    
     update(deltaTime: number) {
 
     }
@@ -8,10 +10,22 @@ export abstract class Component {
 
     // Takes the list of components that the object has 
     // and verifies that it includes all requirements.
-    hasRequirements(components:Component[]): boolean {
-        // TODO Find efficient way of comparing required list to current component list
-        // Perhaps making two arrays of strings from (typeof component)
-        // But I'm not sure how that works with class abtraction 
+
+    // THIS IS UNTESTED DO NOT RELY ON IT
+    hasRequirements(components:Component[]):boolean {
+        if ( this.required_components.length > 0 ){
+            for ( let reqComp of this.required_components ){
+                if ( !this.hasComponent (reqComp, components) ){
+                    throw new Error('Object missing required component {missing_component}, Unable to add {coponent} to object');
+                    return false;
+                }
+            }
+        }
         return true;
+    }
+    hasComponent ( component:Component , component_list:Component[]){
+        for ( let comp of component_list ){
+            if (typeof comp == typeof component) return true;
+        }
     }
 }

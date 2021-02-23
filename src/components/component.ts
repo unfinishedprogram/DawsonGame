2088d84314955dcc32173e65e55d67db03f8a578
nenikitov <nenikitov@mail.ru>
2021-02-23 08:25:14 -0500

    Formated component.ts file to better follow naming and style conventions.

--- src/components/component.ts
+++ src/components/component.ts
@@ -1,34 +1,27 @@
 export abstract class Component {
-    required_components: Component[] | undefined;
-    
-    update(deltaTime: number) {
+	requiredComponents: Component[] | undefined;
 
-    }
+	update(deltaTime: number) {}
 
-    constructor(required_components?: Component[]){
-        this.required_components = required_components;
-    }
+	constructor(requiredComponents?: Component[]) {
+		this.requiredComponents = requiredComponents;
+	}
 
+	// Takes the list of components that the object has
+	// and verifies that it includes all requirements.
 
-    // Takes the list of components that the object has 
-    // and verifies that it includes all requirements.
+	// THIS IS UNTESTED DO NOT RELY ON IT
+	hasRequirements(gameObjectComponents: Component[]): boolean {
+		if (this.requiredComponents)
+			for (let reqComp of this.requiredComponents)
+				if (!this.hasComponent(reqComp, gameObjectComponents))
+					return false;
+		return true;
+	}
 
-    // THIS IS UNTESTED DO NOT RELY ON IT
-    hasRequirements(gameObjectComponents:Component[]): boolean {
-        if ( this.required_components ){
-            for ( let reqComp of this.required_components ){
-                if ( !this.hasComponent(reqComp, gameObjectComponents) ){
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-
-    hasComponent (component:Component, component_list:Component[]): boolean{
-        for ( let comp of component_list ){
-            if (typeof comp == typeof component) return true;
-        }
-        return false;
-    }
+	hasComponent(component: Component, componentList: Component[]): boolean {
+		for (let comp of componentList)
+			if (typeof comp == typeof component) return true;
+		return false;
+	}
 }

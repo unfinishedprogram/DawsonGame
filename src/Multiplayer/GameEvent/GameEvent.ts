71a645eea3887ce1aef05eb238309840938c6e8e
nenikitov <nenikitov@mail.ru>
2021-02-22 21:43:05 -0500

    Changed file and folder names so it follows naming conventions

--- src/Multiplayer/GameEvent/GameEvent.ts
+++
@@ -1,5 +0,0 @@
-export abstract class GameEvent {
-    abstract name: String;
-    //TODO - have a specific data type for ws data!!
-    abstract execute(data: Record<"data" | "value", any>): void;
-}
--- src/Utils/websocket_connect.ts
+++
@@ -1,4 +0,0 @@
-// Guilherme will  add more stuff here later.
-export function create_connection(ip: string, port: number): WebSocket{
-    return new WebSocket(`ws://${ip}:${port}/`);
-}

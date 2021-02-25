a9555a3578c7dd9650f356015fee70d173690919
nenikitov <nenikitov@mail.ru>
2021-02-25 13:30:27 -0500

    Moved the rotation origin of the cube to its center

--- src/objects/presets/cube.ts
+++ src/objects/presets/cube.ts
@@ -15,7 +15,10 @@ export class Cube extends GameObject {
     }
     update(deltaTime: number) {
         let input = this.controller.getInput()
-        this.geometry.translate(input.movementDirection.x * deltaTime * 3, input.movementDirection.y * deltaTime * 3, 0);
-        this.geometry.rotateZ(input.mousePosition.x * 0.01 * deltaTime);
+        this.object3D.position.x += input.movementDirection.x * deltaTime * 3;
+        this.object3D.position.y += input.movementDirection.y * deltaTime * 3;
+        this.object3D.rotation.z += input.mousePosition.x * 0.01 * deltaTime;
+        // this.geometry.translate(input.movementDirection.x * deltaTime * 3, input.movementDirection.y * deltaTime * 3, 0);
+        // this.geometry.rotateZ(input.mousePosition.x * 0.01 * deltaTime);
     }
 }

3f23c7094d07498b8cef0b0a09b894787daa3231
nenikitov <nenikitov@mail.ru>
2021-03-02 09:19:18 -0500

    Added JSDoc to MoreMath

--- src/utils/moreMath.ts
+++ src/utils/moreMath.ts
@@ -1,8 +1,23 @@
+/**
+ * Class that provides more math functions
+ */
 export class MoreMath {
+    /**
+     * Interpolates the number (do not forget to reset the original value)
+     * @param start Original value
+     * @param end Target value
+     * @param mu Interpolation factor (0 - slow, 1 - instant)
+     */
     public static interpolateNumber(start: number, end: number, mu: number) : number {
         return (1 - mu) * start + mu * end;
     }
     
+    /**
+     * Interpolates the angle (do not forget to reset the original value)
+     * @param start Original value
+     * @param end Target value
+     * @param mu Interpolation factor (0 - slow, 1 - instant)
+     */
     public static interpolateAngle(start: number, end: number, mu: number) : number {
 
         if (Math.abs(end - start) > Math.PI) {

e80f1aeb1e3719ce20cb75e3ae90060cd1cc6489
nenikitov <nenikitov@mail.ru>
2021-03-12 00:01:15 -0500

    Changed the double quotes to single ones (yeah it is a filler commit do not judge me I need to follow my green sreak...)

--- src/controller/gamepadObserver.ts
+++ src/controller/gamepadObserver.ts
@@ -3,6 +3,6 @@ import { Observer } from "../utils/observer";
 
 export class GamepadObserver extends Observer<Gamepad> {
     onNotify(action: Action, info: Gamepad): void {
-        console.log("Method not implemented.");
+        console.log('Method not implemented.');
     }
 }
